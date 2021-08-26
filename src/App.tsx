0719264f6f7ca076083c347a318417e20e312d38
Benjamin Michael Baldivia <goblinlordx@gmail.com>
2021-08-27 04:01:09 +0900

    Initial implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { useDecrypt, useEncrypt } from './crypto';
 
 function App() {
+  const [pt, setPt] = useState("");
+  const [password, setPassword] = useState("");
+  const ct = useEncrypt(password, pt)
+
+  const [ct2, setCt2] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [pt2, err] = useDecrypt(password2, ct2)
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <textarea onInput={(e) => setPt(e.currentTarget.value)}></textarea>
+          <textarea
+            onInput={(e) => setPassword(e.currentTarget.value)}
+          ></textarea>
+          <textarea disabled value={ct}></textarea>
+        </div>
+        <hr />
+        <div>
+          <textarea onInput={(e) => setCt2(e.currentTarget.value)}></textarea>
+          <textarea
+            onInput={(e) => setPassword2(e.currentTarget.value)}
+          ></textarea>
+          <textarea disabled value={pt2}></textarea>
+          <div>{err?.message}</div>
+        </div>
       </header>
     </div>
   );
---
+++ src/crypto.tsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+
+const bufToAlpha = (buf: ArrayBuffer) => {
+  const arr = Array.from(new Uint8Array(buf));
+  return btoa(arr.map((byte) => String.fromCharCode(byte)).join(""));
+};
+
+const alphaToBuf = (str: string) =>
+  new Uint8Array(atob(str).split('').map((ch) => ch.charCodeAt(0)));
+
+const encrypt = async (password: string, pt: string) => {
+  const pwHash = await crypto.subtle.digest(
+    "SHA-256",
+    new TextEncoder().encode(password.trim())
+  );
+
+  const iv = crypto.getRandomValues(new Uint8Array(12));
+  const alg = { name: "AES-GCM", iv };
+
+  const key = await crypto.subtle.importKey("raw", pwHash, alg, false, [
+    "encrypt",
+  ]);
+
+  const ctBuf = await crypto.subtle.encrypt(
+    alg,
+    key,
+    new TextEncoder().encode(pt)
+  );
+
+  const ivStr = bufToAlpha(iv);
+  const ctStr = bufToAlpha(ctBuf);
+
+  return `${ivStr}$${ctStr}`;
+};
+
+const decrypt = async (password: string, ctRaw: string): Promise<[string, null | Error]>  => {
+  try {
+    const pwUtf8 = new TextEncoder().encode(password.trim());
+    const pwHash = await crypto.subtle.digest("SHA-256", pwUtf8);
+
+    const [ivStr = "", ctStr = ""] = ctRaw.split("$");
+    const iv = alphaToBuf(ivStr);
+    const ct = alphaToBuf(ctStr);
+
+    const alg = { name: "AES-GCM", iv };
+
+    const key = await crypto.subtle.importKey("raw", pwHash, alg, false, [
+      "decrypt",
+    ]);
+
+    const ptBuf = await crypto.subtle.decrypt(alg, key, ct);
+    return [new TextDecoder().decode(ptBuf), null]
+  } catch (err) {
+    return ['', new Error('Invalid ciphertext or password')]
+  }
+};
+
+export const useEncrypt = (password: string, pt: string) => {
+  const [ct, setCT] = useState("");
+  useEffect(() => {
+    void (async () => {
+      setCT(await encrypt(password, pt));
+    })();
+  }, [password, pt]);
+
+  return ct;
+};
+
+
+export const useDecrypt = (password: string, ct: string) => {
+  const [pt, setPT] = useState("");
+  const [err, setErr] = useState<null | Error>(null);
+  useEffect(() => {
+    void (async () => {
+      const [pt, err] = await decrypt(password, ct)
+      setPT(pt);
+      setErr(err)
+    })();
+  }, [password, ct]);
+
+  return [pt, err] as [string, null | Error];
+};
