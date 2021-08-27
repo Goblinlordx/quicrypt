f443dbb253977cdb77a206fe71a7b199b160d3bd
Benjamin Baldivia <goblinlordx@gmail.com>
2021-08-27 17:24:36 +0900

    add tailwind for styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
 import React from "react";
+
+import "tailwindcss/tailwind.css"
 import "./App.css";
 
 import { Redirect } from "react-router";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 
 import { DecryptView } from "./DecryptView";
 import { EncryptView } from "./EncryptView";
+import { AboutView } from "./AboutView";
 
 function App() {
   return (
-    <div className="App">
+    <div className="min-h-screen bg-indigo-100">
+      <div>
+        <div>Quicrypt</div>
+        <div>
+          <Link to="/encrypt">Encrypt</Link>
+        </div>
+        <div>
+          <Link to="/decrypt">Decrypt</Link>
+        </div>
+        <div>
+          <Link to="/about">About</Link>
+        </div>
+      </div>
       <header className="App-header">
         <Switch>
           <Route path="/encrypt" component={EncryptView} />
           <Route path="/decrypt" component={DecryptView} />
-          <Route path="/about" component={EncryptView} />
+          <Route path="/about" component={AboutView} />
           <Redirect to="/encrypt" />
         </Switch>
       </header>
