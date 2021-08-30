b95f89fefce6848ffe845c6b0ee95d5b720a77ee
Benjamin Michael Baldivia <goblinlordx@gmail.com>
2021-08-30 21:54:39 +0900

    add copy to clipboard functionality

--- src/EncryptView.tsx
+++ src/EncryptView.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useEncrypt } from "./crypto";
+import { toClipboard } from "./toClipboard";
 
 export const EncryptView = () => {
   const [pt, setPt] = useState("");
   const [password, setPassword] = useState("");
   const ct = useEncrypt(password, pt);
 
-  const decryptPath = `/decrypt?ct=${encodeURIComponent(ct)}`;
+  const decryptPath = `${
+    window.location.href.split("#")[0] || ""
+  }#/decrypt?ct=${encodeURIComponent(ct)}`;
 
   return (
     <div className="flex flex-grow flex-col">
@@ -29,6 +32,14 @@ export const EncryptView = () => {
         <div>CipherText: </div>
         <textarea className="input" disabled value={ct}></textarea>
       </label>
+      <button
+        type="button"
+        id="copy"
+        data-clipboard-text={decryptPath}
+        onClick={toClipboard}
+      >
+        Copy URL
+      </button>
       <Link to={decryptPath}>Decryption link</Link>
     </div>
   );
